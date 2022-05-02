861cba1aedc0fda9ecfb74b7a96651edb6c01e8a
Artem024 <alekhinart@yandex.ru>
2022-05-02 19:07:00 +0300

    refactor: чуть подчистил

--- src/Task.java
+++ src/Task.java
@@ -1,18 +1,9 @@
-/*Основной класс задач, в нем должны быть созданы все переменные
-    класса епик и саба
-   Эпик который состоит из саба, который состоит из тасков
-
-    */
-
-
 
 public class Task {
    private String taskName;
    private String taskDescription;
    private int taskId;
    private String taskStatus;
-
-    //enum status {NEW, IN_PROGRESS, DONE };
 
     public Task(String taskName, String taskDescription, int taskId, String taskStatus) {
         this.taskName = taskName;
